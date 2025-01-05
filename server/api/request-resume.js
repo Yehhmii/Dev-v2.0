920565a9a2086825b048f0036a8ff471cd9c06a4
Yehhmii <franciscoabhulimen@gmail.com>
2025-01-04 20:17:53 -0800

    preparing the backend 1

--- server/api/request-resume.js
+++ server/api/request-resume.js
@@ -1,36 +1,37 @@
 const nodemailer = require('nodemailer');
 
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        const { email } = req.body;
-
-        if (!email) {
-            return res.status(400).json({ error: 'Email is required' });
-        }
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
-
-        const mailOptions = {
-            from: email,
-            to: process.env.EMAIL,
-            subject: 'Resume Request',
-            text: `You have a new resume request from: ${email}`,
-        };
-
-        try {
-            await transporter.sendMail(mailOptions);
-            return res.status(200).json({ message: 'Request sent successfully' });
-        } catch (error) {
-            console.error('Error sending message:', error);
-            return res.status(500).json({ error: 'Failed to send request' });
-        }
-    } else {
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
-};
+
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ error: 'Email is required' });
+    }
+
+    // Configuring nodemailer
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL,
+        subject: 'Resume Request',
+        text: `You have a new resume request from: ${email}`,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Request sent successfully' });
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).json({ error: 'Failed to send request' });
+    }
+}
