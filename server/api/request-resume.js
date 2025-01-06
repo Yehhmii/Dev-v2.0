2d6ad2835712f79360cd2631e8515ad795c03071
Yehhmii <franciscoabhulimen@gmail.com>
2025-01-05 22:15:16 -0800

    preparing the backend 10

--- server/api/request-resume.js
+++ server/api/request-resume.js
@@ -1,14 +1,7 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
-    // Set CORS headers
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', 'https://dev-v2-1.vercel.app'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+
 
     if (req.method === 'OPTIONS') {
         // Handle preflight request
